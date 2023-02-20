9a11f888fb396118d17b0d925a6be2bf922248eb
Dlison24 <dilacn97@hotmail.com>
2023-02-20 14:15:53 -0500

    [NEW]-add exercise

---
+++ C++/Lectua o entrada de datos.cpp
@@ -0,0 +1,18 @@
+//Lectura o entrada de datos
+
+#include<iostream>
+
+using namespace std;
+
+int main (){
+	
+	cout<<"----LECTURA O ENTRADA DE DATOS----"<<endl;cout<<endl;
+	float numero; //definiendo la variable
+	
+	cout<<"Digite un numero:  ";
+	cin>>numero;  //guardar la variable 
+	
+	cout<<"\nEl numero que digito es:  "<<numero;
+	
+	return 0; 
+}

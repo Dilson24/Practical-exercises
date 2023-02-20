dc0725b824d5135fd065480f78f4690cbafc6217
Dlison24 <dilacn97@hotmail.com>
2023-02-20 16:56:03 -0500

    [NEW]-add exercise

---
+++ C++/Programa que escribe los numeros del 1 al 10 (ciclo while).cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+/*Hacer un programa que
+escriba los números de 1 al 10*/
+
+using namespace std;
+
+int main (){
+	cout<<"PROGRAMA QUE ESCRIBE LOS NUMEROS 1 AL 10"<<endl;cout<<endl;
+    int i;
+	
+	i=1;
+	
+	while(i<=10){
+		cout<<i<<endl; 
+		i++;
+	}
+	
+	return 0;
+}
+	

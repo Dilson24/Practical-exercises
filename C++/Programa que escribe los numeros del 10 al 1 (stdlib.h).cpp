2fbe3aef2811e232ec410a1a1a826ba18ad48de9
Dlison24 <dilacn97@hotmail.com>
2023-02-20 18:50:05 -0500

    [NEW]-add exercise

---
+++ C++/Programa que escribe los numeros del 10 al 1 (stdlib.h).cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<stdlib.h>
+
+using namespace std;
+
+int main(){
+	cout<<"---PROGRAMA QUE ESCRIBE LOS NUMEROS 10 AL 1---"<<endl;cout<<endl;
+	int i;
+	
+	i=10;
+	
+	while(i>=1){
+		cout<<i<<endl;
+		i--;
+	}
+	
+	system("pause");
+	return 0;
+}

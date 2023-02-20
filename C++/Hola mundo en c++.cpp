626499050b8a504503837d65fb93a49b74d88ef1
Dlison24 <dilacn97@hotmail.com>
2023-02-20 14:08:32 -0500

    [NEW]-add exercise

---
+++ C++/Hola mundo en c++.cpp
@@ -0,0 +1,12 @@
+//Primer programa en c++
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    cout<<"----PRIMER PROGRAMA EN C++----"<<endl;cout<<endl;
+    cout<<"Hola mundo :v"<<endl;
+	
+	return 0;
+}

187dc4405c4ba8d32bde362262071decdb1730f2
Dlison24 <dilacn97@hotmail.com>
2023-02-20 14:13:50 -0500

    [NEW]-add exercise

---
+++ C++/Tipos de datos en c++.cpp
@@ -0,0 +1,16 @@
+//Tipos de datos basicos en c++
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+	cout<<"--------TIPOS DE DATOS BASICOS EN C++---------"<<endl;cout<<endl;
+	int entero = 15 ;
+	float flotante = 10.45;
+	double mayor = 16.3456;
+	char letra = '1';
+	
+	cout<<letra;
+	return 0;
+}

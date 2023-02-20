c6aac02e43668ea4df26eb6aa02cc74dc2b16f7f
Dlison24 <dilacn97@hotmail.com>
2023-02-20 15:19:01 -0500

    [NEW]-add exercise

---
+++ C++/Programa calcular el valor de tres notas.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+
+/*La nota final de un estudiante es la media ponderada de 3 notas. La nota de practica que
+equivale a un 30% del total, la nota teórica que equivale a un 60% y la nota de
+participación que equivale a un 10% restante.Escriba un programa que lea de la entrada
+estándar las 3 notas y escriba la salida estándar de la nota final.*/
+
+using namespace std;
+
+int main(){
+	cout<<"---PROGRAMA QUE LEE 3 NOTAS Y MUESTRA SU SALIDA ESTANDAR---"<<endl;cout<<endl;
+    float practica, teorica,participacion,nota_final=0;
+	
+	cout<<"Digite la nota de practica: "; cin >>practica;
+	cout<<"Digite la nota teorica: "; cin >> teorica;
+	cout<<"Digite la nota participacion: "; cin >>participacion;
+
+	
+	practica*= 0.30; //practica = practica * 0.30
+	teorica *= 0.60;
+	participacion *= 0.10;
+
+	nota_final  = practica + teorica + participacion; 
+	 
+	cout<<"\nEL nota final es: "<<nota_final;
+	
+	
+	return 0;
+}

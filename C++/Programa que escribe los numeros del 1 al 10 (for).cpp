49b69d7f1db6a7cad43dd62c344020cb625d7b32
Dlison24 <dilacn97@hotmail.com>
2023-02-20 19:03:03 -0500

    [NEW]-add exercise

---
+++ C++/Programa que escribe los numeros del 1 al 10 (for).cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+	
+int main(){
+	cout<<"PROGRAMA QUE ESCRIBE LOS NUMEROS 1 AL 10"<<endl;cout<<endl;
+	int i;
+	
+	for(i=1; i<=10; i++){
+		cout<<i<<endl;
+	}
+	getch();
+	return 0;
+}

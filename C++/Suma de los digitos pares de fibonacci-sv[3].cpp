d81ce86d83b88f1a512dbc97c2b109cf5767346b
Dlison24 <dilacn97@hotmail.com>
2023-02-20 19:46:38 -0500

    [NEW]-add exercise

---
+++ C++/Suma de los digitos pares de fibonacci-sv[3].cpp
@@ -0,0 +1,23 @@
+/*Sumar los números pares de la serie fibonacci
+que no exeda el número cuatro millones, 
+usando recursion*/
+
+#include <iostream>
+using namespace std;
+int fib(int x){
+	if((x==1)||(x==0)){
+		return(x*2);
+		
+	}else{
+		return(4*fib(x-1)+fib(x-2));
+	}
+}
+int main(){
+	int i=0,limite=4000000, suma =0;
+	while(fib(i)<limite){
+		suma=suma+fib(i);
+		i++;
+	}
+	std::cout <<suma<<std::endl;
+	return 0;
+}

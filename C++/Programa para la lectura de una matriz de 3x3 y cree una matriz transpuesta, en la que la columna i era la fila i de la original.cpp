a0868f3deb92dbe5300c20c34f453110ef639179
Dlison24 <dilacn97@hotmail.com>
2023-02-22 17:16:54 -0500

    [NEW]-add exercise

---
+++ C++/Programa para la lectura de una matriz de 3x3 y cree una matriz transpuesta, en la que la columna i era la fila i de la original.cpp
@@ -0,0 +1,36 @@
+/*Programa para la lectura de una matriz de 3x3 y cree una matriz transpuesta, en la que la columna i era la fila i de la original.*/
+
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+int main(){
+	int numeros[3][3];
+	
+	for(int i=0;i<3;i++){
+		for(int j=0;j<3;j++){
+			cout<<"Digite un número ["<<i<<"]["<<j<<"]: ";
+			cin>>numeros[i][j];
+		}
+	}
+	
+	cout<<"Matriz original\n";
+	for(int i=0;i<3;i++){
+		for(int j=0;j<3;j++){
+			cout<<numeros[i][j];
+		}
+		cout<<"\n";
+	}
+	
+	cout<<"\nMatriz transpuesta\n";
+	for(int i=0;i<3;i++){
+		for(int j=0;j<3;j++){
+			cout<<numeros[j][i];
+		}
+		cout<<"\n";
+	}
+	
+	getch();
+	return 0;
+}

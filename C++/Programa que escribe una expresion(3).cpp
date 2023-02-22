a137ed6ab7cf22a35b7fb4e5611564a394e48198
Dlison24 <dilacn97@hotmail.com>
2023-02-22 15:48:22 -0500

    [NEW]-add exercise

---
+++ C++/Programa que escribe una expresion(3).cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+/*Escribe la siguiente expresión matemática como expresión en C++:(A+B) / (C+D)*/
+
+using namespace std;
+int main() {  
+   cout<<"PROGRAMA QUE CALCULA LA EXPRESION (A+B) / (C+D). "<<endl;cout<<endl;
+
+   float a,b,c,d,resultado = 0;
+
+    
+   cout<<"DIGITE LA EXPRECION DE (A+B) / (C+D)\n";
+   cout<<"Digite el valor de (a):";
+   cin>>a;
+   cout<<"Digite el valor de (b):";
+   cin>>b;
+   cout<<"Digite el valor de (c):";
+   cin>>c;
+   cout<<"Digite el valor de (d):";
+   cin>>d;
+   resultado = (a+b) / (c+d);
+   cout.precision(3);
+   cout<<"El resultado es:" <<resultado<<endl;
+   
+ return 0;
+
+}
